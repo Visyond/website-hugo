c7af09d180af7dc4732c161602660f7a0d20d549
ilajosmanov <illiaosmanov@gmail.com>
2017-12-29 03:15:38 +0200

    added first policy

---
+++ site/content/privacy.md
@@ -0,0 +1,9 @@
+---
+title: Privacy policy
+type: privacy
+page: /privacy.html
+image: /img/vimages/topImgPrivacy.png
+description: >-
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+
+---
---
+++ src/js/cms-preview-templates/privacy.js
@@ -0,0 +1,30 @@
+import React from "react";
+import format from "date-fns/format";
+
+export default class Policy extends React.Component {
+	render() {
+		const {entry, widgetFor, getAsset} = this.props;
+    	let image = getAsset(entry.getIn(["data", "image"]));
+
+    	return <div>
+	    	<section className="titleImage privacyPol">
+	    		<div className="titleImageWrapper container-fluid" style={{backgroundImage: image && `url(${image})`}}>
+		            <div className="row">
+		                <h1 className="pagesTitle">{ entry.getIn(["data", "title"]) }</h1>
+		            </div>
+		        </div>
+	    	</section>
+	    	<section className="privacyPolicy">
+	    		<div className="privacyPolicyWrapper container">
+	    			<div className="row">
+	    				<div className="col-md-2"></div>
+	    					<div className="col-md-8">
+								{ widgetFor("body") }
+	    					</div>
+	    				<div className="col-md-2"></div>
+	    			</div>
+	    		</div>
+	    	</section>
+    	</div>;
+	}
+}
