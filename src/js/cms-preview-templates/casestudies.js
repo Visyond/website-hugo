8267da1dcc4666b4643abbddb68024d0a4447f7e
ilajosmanov <illiaosmanov@gmail.com>
2018-01-31 16:48:36 +0200

    casestudy all

--- src/js/cms-preview-templates/casestudies.js
+++ src/js/cms-preview-templates/casestudies.js
@@ -2,5 +2,12 @@ import React from 'react';
 import format from 'date-fns/format';
 
 export default class CaseStudies extends React.Component {
+    render () {
+        const {entry, getAsset, widgetFor} = this.props;
+        let image = getAsset(entry.getIn(["data", "image"]));
 
+        return <div>
+            lorem
+        </div>
+    };
 }
