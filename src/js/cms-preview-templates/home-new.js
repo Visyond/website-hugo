8006ee0eb88f0befd13e92388eeccdf6014bf4c2
dmnmkua <dmn.mk.ua@gmail.com>
2018-04-26 22:12:52 +0300

    test - add cms

---
+++ src/js/cms-preview-templates/home-new.js
@@ -0,0 +1,278 @@
+import React from 'react';
+import format from 'date-fns/format';
+export default class HomeNew extends React.Component {
+    render () {
+        const { entry, widgetFor, getAsset, widgetsFor } = this.props;
+        let image = getAsset(entry.getIn(["data", "topImage"]));
+
+        return (
+            <div>
+            <div className="new-home nh">
+
+              <!-- top section -->
+              <div className="nh-top">
+                <h2 className="nh-top__title">
+                  single source of truth for your spreadsheets
+                </h2>
+
+                <p className="nh-top__under-title">
+                  Visyond is a collaborative speadsheet platform that reduced security threats and errors, speeds up the decision making process, adds secure selective sharing, data traking
+                </p>
+
+                <div className="nh-top__btns-wrap">
+                  <button className="nh-link nh-link--sign-up">
+                    sign up
+                  </button>
+
+                  <button className="nh-link nh-link--request">
+                    request a demo
+                  </button>
+                </div>
+
+                <img src="/img/391.jpg" alt="image" className="nh-top__img">
+              </div>
+              <!-- ./top section -->
+
+              <!-- trusted section -->
+              <section className="nh-trusted">
+                <h3 className="nh-trusted__title">
+                  trusted by
+                </h3>
+
+                <div className="nh-trusted__logos-wrap">
+                  <img src="/img/home/elt.png" alt="elt electronica">
+                  <img src="/img/home/ericsson.png" alt="ericsson">
+                  <img src="/img/home/deloitte.png" alt="deloitte">
+                  <img src="/img/home/abrt.png" alt="abrt">
+                  <img src="/img/home/jebg.png" alt="jebg">
+                </div>
+              </section>
+              <!-- ./trusted section -->
+
+              <!-- section info -->
+              <div className="nh-info">
+
+                <!-- info block -->
+                <div className="nh-info__block">
+                  <img src="/img/home/v_clip_group_1.png" alt="collaborate on spreadsheets" className="nh-info__block-img">
+
+                  <img src="/img/home/lines.png" alt="lines" className="nh-info__block-lines">
+
+                  <section className="nh-info__text-wrap">
+                    <h3 className="nh-info__block-title">
+                      collaborate on spreadsheets
+                    </h3>
+
+                    <p className="nh-info__block-descr">
+                      ...without back-and-forth emails and data chaos and enjoy the benefits of <span>data tracking, version control and secure selective sharing</span> - allow access to only certain cells, worksheets, or linked reports and slides.
+                    </p>
+
+                    <button className="nh-link nh-link--learn-more">
+                      learn more
+                    </button>
+                  </section>
+                </div>
+                <!-- ./info block -->
+
+                <!-- info block -->
+                <div className="nh-info__block">
+                  <img src="/img/home/v_clip_group_2.png" alt="collaborate on spreadsheets" className="nh-info__block-img">
+
+                  <img src="/img/home/lines.png" alt="lines" className="nh-info__block-lines">
+
+                  <section className="nh-info__text-wrap">
+                    <h3 className="nh-info__block-title">
+                      build robust model
+                    </h3>
+
+                    <p className="nh-info__block-descr">
+                      ...as a team without the risk of accidentally overriding data or formulas with Visyond's <span>granular permissions control</span>, support of different coexisting cases, <span>scenario comparison</span>, dependency & errors root cause analysis.
+                    </p>
+
+                    <button className="nh-link nh-link--learn-more">
+                      learn more
+                    </button>
+                  </section>
+                </div>
+                <!-- ./info block -->
+
+                <!-- info block -->
+                <div className="nh-info__block">
+                  <img src="/img/home/v_clip_group_3.png" alt="collaborate on spreadsheets" className="nh-info__block-img">
+
+                  <img src="/img/home/lines.png" alt="lines" className="nh-info__block-lines">
+
+                  <section className="nh-info__text-wrap">
+                    <h3 className="nh-info__block-title">
+                      analyze data
+                    </h3>
+
+                    <p className="nh-info__block-descr">
+                      ...and <span>in a few clicks gain valuable insights</span> by performing advanced analysis (Sensitivity, Scenario and many more!). Add the charts to Visyond's interactive slides and <span>share only what each stakeholder needs to see</span>.
+                    </p>
+
+                    <button className="nh-link nh-link--learn-more">
+                      learn more
+                    </button>
+                  </section>
+                </div>
+                <!-- ./info block -->
+
+                <!-- info block -->
+                <div className="nh-info__block">
+                  <img src="/img/home/v_clip_group_4.png" alt="collaborate on spreadsheets" className="nh-info__block-img">
+
+                  <section className="nh-info__text-wrap">
+                    <h3 className="nh-info__block-title">
+                      share the results
+                    </h3>
+
+                    <p className="nh-info__block-descr">
+                      ...safely without the risk of damaging or losing any data. Prepare <span>data visualizations, presentations and reports</span> in a few simple steps. <span>Create dashboards &amp; apps</span> powered by the spreadsheet calculation engine <span>without programming</span>.
+                    </p>
+
+                    <button className="nh-link nh-link--learn-more">
+                      learn more
+                    </button>
+                  </section>
+                </div>
+                <!-- ./info block -->
+
+              </div>
+              <!-- ./section info -->
+
+              <!-- section capabilities -->
+              <section className="nh-capabilities">
+                <h3 className="nh-capabilities__title">
+                  With Visyond you can
+                </h3>
+
+                <div className="nh-capabilities__modules">
+                  <div className="nh-capabilities__block">
+                    <img src="/img/home/v_icon.png" alt="save time" className="nh-capabilities__img">
+
+                    <p className="nh-capabilities__descr">
+                      Save up to <span>80%</span> of time
+                    </p>
+                  </div>
+
+                  <div className="nh-capabilities__block">
+                    <img src="/img/home/v_icon2.png" alt="reduce costs" className="nh-capabilities__img">
+
+                    <p className="nh-capabilities__descr">
+                      Reduce costs up to <span>$420</span>
+                    </p>
+                  </div>
+
+                  <div className="nh-capabilities__block">
+                    <img src="/img/home/v_icon3.png" alt="reduce error" className="nh-capabilities__img">
+
+                    <p className="nh-capabilities__descr">
+                      Reduce errors by <span>90%</span>
+                    </p>
+                  </div>
+                </div>
+              </section>
+              <!-- ./section capabilities -->
+
+              <!-- section studies -->
+              <section className="nh-studies">
+                <h3 className="nh-studies__title">
+                  Featured case studies
+                </h3>
+
+                <!-- first block -->
+                <div className="nh-studies__block">
+                  <div className="nh-studies__img-wrap">
+                    <img src="/img/home/v_case_1.png" alt="case" className="nh-studies__img">
+                    <span className="nh-studies__img-text nh-studies__img-text--first">from 7 weeks</span>
+                    <span className="nh-studies__img-text nh-studies__img-text--second">to 2 hours</span>
+                  </div>
+
+                  <section className="nh-studies__text-wrap">
+                    <h3 className="nh-studies__block-title">
+                      Elettronica reduced its sales planning cycle
+                    </h3>
+
+                    <cite className="nh-studies__block-descr">
+                      When i used Visyond, I see my accuracy and ability to answer questions about my work go up. I can do more and have peace of mind.
+                    </cite>
+
+                    <p className="nh-studies__block-author">
+                      Fernando Rizzo, Area Manager, Elettronica
+                    </p>
+
+                    <a href="#" className="nh-studies__block-link">
+                      learn more>>
+                    </a>
+                  </section>
+                </div>
+                <!-- ./first block -->
+
+                <!-- second block -->
+                <div className="nh-studies__block">
+                  <div className="nh-studies__img-wrap">
+                    <img src="/img/home/v_case_2.png" alt="case" className="nh-studies__img">
+                    <span className="nh-studies__img-text nh-studies__img-text--third">from 2 weeks</span>
+                    <span className="nh-studies__img-text nh-studies__img-text--fourth">to 1 hour</span>
+                  </div>
+
+                  <section className="nh-studies__text-wrap">
+                    <h3 className="nh-studies__block-title">
+                      Now eLearnSecurity decreased time spent for financial statements preparation
+                    </h3>
+
+                    <cite className="nh-studies__block-descr">
+                      When i used Visyond, I see my accuracy and ability to answer questions about my work go up. I can do more and have peace of mind.
+                    </cite>
+
+                    <p className="nh-studies__block-author">
+                      Fernando Rizzo, Area Manager, Elettronica
+                    </p>
+
+                    <a href="#" className="nh-studies__block-link">
+                      learn more>>
+                    </a>
+                  </section>
+                </div>
+                <!-- ./second block -->
+              </section>
+              <!-- ./section studies -->
+
+              <!-- section provides -->
+              <div className="nh-provides">
+                <section className="nh-provides__wrap">
+                  <h3 className="nh-provides__title">
+                    Visyond provides
+                  </h3>
+
+                  <ul className="nh-provides__list">
+                    <li className="nh-provides__item">
+                      A flexible and familiar spreadsheet inter-face that is <span>cross-compatible</span> with Excel.
+                    </li>
+                    <li className="nh-provides__item">
+                      Low entry barrier - Visyond is easy <span>to use and learn</span>.
+                    </li>
+                    <li className="nh-provides__item">
+                      Low entry barrier - Visyond is easy <span>to use and learn
+                    </li>
+                  </ul>
+                </section>
+              </div>
+              <!-- ./section provides -->
+
+              <section className="nh-bottom">
+                <h3 className="nh-bottom__title">
+                  Dramatically improve your decision-making process today!
+                </h3>
+
+                <button className="nh-link nh-link--request">
+                  request a demo
+                </button>
+              </section>
+
+            </div>
+            </div>
+        );
+    }
+}
