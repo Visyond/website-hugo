d4fca448a0cc772e4128fd4d0940354e9012f572
w96k <w96k.ru@gmail.com>
2018-11-15 14:53:29 +0300

    Format package.json & gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,98 +15,112 @@ import pngquant from "imagemin-pngquant";
 import csso from "postcss-csso";
 
 const browserSync = BrowserSync.create();
-const hugoBin = `./bin/hugo.${process.platform === "win32" ? "exe" : process.platform}`;
+const hugoBin = `./bin/hugo.${
+  process.platform === "win32" ? "exe" : process.platform
+}`;
 const defaultArgs = ["-d", "../dist", "-s", "site"];
 
-gulp.task("hugo", (cb) => buildSite(cb));
-gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
+gulp.task("hugo", cb => buildSite(cb));
+gulp.task("hugo-preview", cb =>
+  buildSite(cb, ["--buildDrafts", "--buildFuture"])
+);
 gulp.task("build", ["css", "js", "cms-assets", "hugo", "img"]);
 gulp.task("build-preview", ["css", "js", "cms-assets", "hugo-preview"]);
 
-gulp.task("css", () => (
-    gulp.src("./src/css/*.css")
-        .pipe(postcss([
-            cssImport({from: "./src/css/main.css"}),
-            cssnext(),
-            csso({
-                forceMediaMerge: true,
-                clone: true,
-                comments: false,
-            }),
-        ]))
-        .pipe(gulp.dest("./dist/css"))
-        .pipe(browserSync.stream())
-));
+gulp.task("css", () =>
+  gulp
+    .src("./src/css/*.css")
+    .pipe(
+      postcss([
+        cssImport({ from: "./src/css/main.css" }),
+        cssnext(),
+        csso({
+          forceMediaMerge: true,
+          clone: true,
+          comments: false
+        })
+      ])
+    )
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream())
+);
 
-gulp.task("cms-assets", () => (
-    gulp.src("./node_modules/netlify-cms/dist/*.{woff,eot,woff2,ttf,svg,png}")
-        .pipe(gulp.dest("./dist/css"))
-));
+gulp.task("cms-assets", () =>
+  gulp
+    .src("./node_modules/netlify-cms/dist/*.{woff,eot,woff2,ttf,svg,png}")
+    .pipe(gulp.dest("./dist/css"))
+);
 
-gulp.task("js", (cb) => {
-    const myConfig = Object.assign({}, webpackConfig);
+gulp.task("js", cb => {
+  const myConfig = Object.assign({}, webpackConfig);
 
-    webpack(myConfig, (err, stats) => {
-        if (err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            colors: true,
-            progress: true
-        }));
-        browserSync.reload();
-        cb();
-    });
+  webpack(myConfig, (err, stats) => {
+    if (err) throw new gutil.PluginError("webpack", err);
+    gutil.log(
+      "[webpack]",
+      stats.toString({
+        colors: true,
+        progress: true
+      })
+    );
+    browserSync.reload();
+    cb();
+  });
 });
 
 gulp.task("svg", () => {
-    const svgs = gulp
-          .src("site/static/img/vimages/*.svg")
-          .pipe(svgmin())
-          .pipe(svgstore({inlineSvg: true}));
+  const svgs = gulp
+    .src("site/static/img/vimages/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({ inlineSvg: true }));
 
-    function fileContents(filePath, file) {
-        return file.contents.toString();
-    }
+  function fileContents(filePath, file) {
+    return file.contents.toString();
+  }
 
-    return gulp
-        .src("site/layouts/partials/svg.html")
-        .pipe(inject(svgs, {transform: fileContents}))
-        .pipe(gulp.dest("site/layouts/partials/"));
+  return gulp
+    .src("site/layouts/partials/svg.html")
+    .pipe(inject(svgs, { transform: fileContents }))
+    .pipe(gulp.dest("site/layouts/partials/"));
 });
 
 gulp.task("img", () => {
-    return gulp.src('site/static/source-img/*')
-        .pipe(imagemin({
-            interlaced: true,
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('site/static/img'))
-        .pipe(browserSync.stream())
-})
+  return gulp
+    .src("site/static/source-img/*")
+    .pipe(
+      imagemin({
+        interlaced: true,
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        use: [pngquant()]
+      })
+    )
+    .pipe(gulp.dest("site/static/img"))
+    .pipe(browserSync.stream());
+});
 
 gulp.task("server", ["hugo", "css", "cms-assets", "js", "svg"], () => {
-    browserSync.init({
-        server: {
-            baseDir: "./dist"
-        }
-    });
-    gulp.watch("./src/js/**/*.js", ["js"]);
-    gulp.watch("./src/css/**/*.css", ["css"]);
-    gulp.watch("./site/static/img/vimages/*.svg", ["svg"]);
-    gulp.watch("./site/**/*", ["hugo"]);
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  gulp.watch("./src/js/**/*.js", ["js"]);
+  gulp.watch("./src/css/**/*.css", ["css"]);
+  gulp.watch("./site/static/img/vimages/*.svg", ["svg"]);
+  gulp.watch("./site/**/*", ["hugo"]);
 });
 
 function buildSite(cb, options) {
-    const args = options ? defaultArgs.concat(options) : defaultArgs;
+  const args = options ? defaultArgs.concat(options) : defaultArgs;
 
-    return cp.spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
-        if (code === 0) {
-            browserSync.reload("notify:false");
-            cb();
-        } else {
-            browserSync.notify("Hugo build failed :(");
-            cb("Hugo build failed");
-        }
-    });
+  return cp.spawn(hugoBin, args, { stdio: "inherit" }).on("close", code => {
+    if (code === 0) {
+      browserSync.reload("notify:false");
+      cb();
+    } else {
+      browserSync.notify("Hugo build failed :(");
+      cb("Hugo build failed");
+    }
+  });
 }
