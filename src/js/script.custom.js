cde192b15b70970e148bbbaaca2bf5ef35eddfdb
alexeyvisyond <alex@visyond.com>
2021-05-26 20:58:50 +0300

    aaa

--- src/js/script.custom.js
+++ src/js/script.custom.js
@@ -165,7 +165,7 @@ $(function () {
     
         //=====Toggle opening of demo video lists=====
         (function () {
-            let toggles = $('.demoSection__body');
+            let toggles = $('.demoExpandingSection');
             toggles.each(function () {
                 $(this).click(function () {
                     $(this).toggleClass('opened');
