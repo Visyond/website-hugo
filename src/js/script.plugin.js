6e7b915d327c63a19f5027dea6c918d4a0a2f616
Alex <alex@visyond.com>
2022-10-29 01:32:43 +0300

    use cases sliders here goes nothing

--- src/js/script.plugin.js
+++ src/js/script.plugin.js
@@ -101,4 +101,46 @@ $(function () {
         });
     }
     */
+
+    if($('.js-useCases__list').length) {
+      $('.js-useCases__list').slick({
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          autoplay: false,
+          autoplaySpeed: 2000,
+          dots: false,
+          arrows: false,
+          responsive: [
+              {
+                breakpoint: 1366,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true,
+                }
+              },
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: true,
+                  dots: true,
+                }
+              },
+              {
+                breakpoint: 691,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true,
+                }
+              },
+          ]
+      })
+  }    
+
+    
 });
