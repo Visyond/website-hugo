1f66252e44374257ecb717de780d452789f4ffd5
Alex <alex@visyond.com>
2022-10-31 16:51:55 +0300

    home slider

--- src/js/script.plugin.js
+++ src/js/script.plugin.js
@@ -102,48 +102,9 @@ $(function () {
     }
     */
 
-    if($('.js-useCases__list').length) {
-      $('.js-useCases__list').slick({
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          autoplay: false,
-          autoplaySpeed: 3000,
-          dots: true,
-          arrows: true,
-          responsive: [
-              {
-                breakpoint: 1366,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-                  infinite: true,
-                  dots: true,
-                }
-              },
-              {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-                  infinite: true,
-                  dots: true,
-                }
-              },
-              {
-                breakpoint: 691,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1,
-                  infinite: true,
-                  dots: true,
-                }
-              },
-          ]
-      })
-  }    
 
-  if($('.js-useCases__listTEST').length) {
-    $('.js-useCases__listTEST').slick({
+  if($('.js-useCases__list').length) {
+    $('.js-useCases__list').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
         autoplay: false,
