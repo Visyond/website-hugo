ba496c5c6c05aca4111239413a816b81fe6c878d
Abderrahmane Boulahdour <58618795+Arboulahdour@users.noreply.github.com>
2022-11-23 21:08:48 +0100

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -2,14 +2,12 @@
 
 REGISTRY_USER=${{ secrets.REGISTRY_USER }}
 REGISTRY_PASSWORD=${{ secrets.REGISTRY_PASSWORD }}
-IMAGE_TAG=$(git rev-parse --short "${GITHUB_SHA}")
+IMAGE_TAG=$(git rev-parse --short "${GITHUB_SHA::6}")
 IMAGE_NAME="visyond-cms"
 
 # Building/Pushing the Docker image
 docker login -u $REGISTRY_USER -p $REGISTRY_PASSWORD
 
 sudo docker build -t $REGISTRY_USER/$IMAGE_NAME:$IMAGE_TAG -f docker/Dockerfile.dev .
-sudo docker tag $REGISTRY_USER/$IMAGE_NAME:$IMAGE_TAG $REGISTRY_USER/$IMAGE_NAME:latest
-
 sudo docker push $REGISTRY_USER/$IMAGE_NAME:$IMAGE_TAG
-sudo docker push $REGISTRY_USER/$IMAGE_NAME:latest
+
