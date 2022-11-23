63290e0df9f3ba58d75392dfc50d1e4d00384f40
Abderrahmane Boulahdour <58618795+Arboulahdour@users.noreply.github.com>
2022-11-23 17:21:45 +0100

    Create docker-build.sh

---
+++ docker-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOCKER_USER="visyond"
+DOCKER_PASSWORD=""
+GITHUB_HASH=$(git rev-parse --short "${GITHUB_SHA::6}")
+
+echo "hash: " $GITHUB_HASH
+
+# Adding docker-ce repository
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+echo
+
+# Installing docker-ce and docker-ce-cli
+sudo apt install -y docker-ce docker-ce-cli
+sudo systemctl enable docker
+sudo systemctl start docker
+echo
+
+sudo docker build -t $DOCKER_USER/visyond-cms:latest -f docker/Dockerfile .
